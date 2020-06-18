dcfe47e4cafc489cd3870b85a6860f5a8e61cebf
RodrigoGR <rodrigo.gomez@vasslatam.com>
2020-06-18 18:06:10 -0500

    mergesssssss

--- server.js
+++ server.js
@@ -36,14 +36,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(cookieParser())
 
-app.all('*', function(req, res, next) {
-  var origin = req.get('origin'); 
-  res.header('Access-Control-Allow-Origin', origin);
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
 // Set static folder
 app.use(express.static(path.join(__dirname, '/dist')));
 
