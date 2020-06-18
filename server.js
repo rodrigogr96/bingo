1c2e18941314d37477a45506d959a8f0765ba0a1
RodrigoGR <rodrigo.gomez@vasslatam.com>
2020-06-18 15:36:43 -0500

    merge

--- server.js
+++ server.js
@@ -123,6 +123,6 @@ io.on('connection', socket => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(port, () => console.log(`Server running on port ${PORT}`));
