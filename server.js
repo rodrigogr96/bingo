e66caf6a561d73d31acb3f7f39d0dd0d35cd0b39
RodrigoGR <rodrigo.gomez@vasslatam.com>
2020-06-18 19:02:27 -0500

    mergesssssssss

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use('/bingo',cors(corsOptions), indexRouter)
+app.use('/bingo', indexRouter)
 
 // Set static folder
 app.use(express.static(path.join(__dirname, '/dist')));
