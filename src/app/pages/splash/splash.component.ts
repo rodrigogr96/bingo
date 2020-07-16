31274916ed5bcf9e677b443fc329b4f2e39bd4b6
RodrigoGR <rodrigo.gomez@vasslatam.com>
2020-07-15 22:18:57 -0500

    merge

--- src/app/pages/splash/splash.component.ts
+++ src/app/pages/splash/splash.component.ts
@@ -11,15 +11,19 @@ export class SplashComponent implements OnInit {
   constructor(private router:Router) { }
 
   ngOnInit() {
-    if(localStorage.getItem('phone') && localStorage.getItem('user')){
-      setTimeout(() => {
-        this.router.navigate([`/home`],{replaceUrl:true})
-      }, 5000);
-    }else{
-      setTimeout(() => {
-        this.router.navigate([`/user`],{replaceUrl:true})
-      }, 5000);
-    }
+    // if(localStorage.getItem('phone') && localStorage.getItem('user')){
+    //   setTimeout(() => {
+    //     this.router.navigate([`/home`],{replaceUrl:true})
+    //   }, 5000);
+    // }else{
+    //   setTimeout(() => {
+    //     this.router.navigate([`/user`],{replaceUrl:true})
+    //   }, 5000);
+    // }
+    window.location.replace("app://movistar/login"); 
+    setTimeout(function () {
+    window.location.replace("https://play.google.com/store/apps/details?id=tdp.app.col&hl=es_PE"); 
+    }, 2000);
     
   }
 
